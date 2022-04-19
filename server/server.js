dcdfbadfa1f14e11e8d61c9f2e0920a1ed8cf84f
kamil-ciesla <kamil.ciesla.dev@gmail.com>
2022-04-19 15:43:36 +0200

    backend cors allow all

--- server/server.js
+++ server/server.js
@@ -3,24 +3,10 @@ const bodyParser = require("body-parser");
 const Customer = require("./api/customer");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+const port = process.env.PORT || 5000;
 const jsvat = require("jsvat");
 
-
-const whitelist = ["http://localhost:3000"];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 const jsonParser = bodyParser.json()
 //const urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -30,9 +16,9 @@ const jsonParser = bodyParser.json()
 const customers = [];
 
 const testCustomersData = [
-    { name: "Oliver Cole", vatNumber: "gb1234567890", countryCode: 'GB', address: "Petticoat Lane 707", city: 'London' },
     { name: "Jan Kowalski", vatNumber: "pl4545454601", countryCode: 'PL', address: "Mokotów 15", city: 'Warsaw' },
     { name: "Sheila Dorman", vatNumber: "cn546738292", countryCode: 'CN', address: "Xin Zhen Lu 973", city: 'Shanghai' },
+    { name: "John Smith", vatNumber: "gb1234567890", countryCode: 'GB', address: "Petticoat Lane 707", city: 'London' },
     { name: "Dave Jenkins", vatNumber: "pl847882100", countryCode: 'FR', address: "Wielopole 1", city: 'Kraków' },
 ]
 for (const customerData of testCustomersData) {
