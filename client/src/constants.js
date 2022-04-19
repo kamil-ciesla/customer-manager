7334249f5daa76dae998c2387b15aa2601cc62b1
kamil-ciesla <kamil.ciesla.dev@gmail.com>
2022-04-19 17:43:05 +0200

    changed server base link

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,1 @@
-export const SERVER_BASE_LINK = process.env.BACKEND_SERVER_LINK || 'http://localhost:5000'
+export const SERVER_BASE_LINK = 'https://express-vercel-9n2z5lq2c-kamil-ciesla.vercel.app'
